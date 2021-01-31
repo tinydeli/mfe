769345e8cc19f8028eb8e823b5e0939ccc8e721f
gvisbeen <gvisbeen@gmail.com>
2021-01-31 13:47:41 +0100

    Changed Homepage title

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              TinyDeli Home Page
             </Typography>
             <Typography
               variant="h5"
