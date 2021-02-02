9324d3261b8a83d376c6adcf0f0ec5c0f7786887
Gideon Visbeen <gvisbeen@client20.ict.fqdn>
2021-02-02 15:27:58 +0100

    DIV id removed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+//92.65.232.88 ip work
+//85.149.29.94 ip home
+
 export default () => {
     return (
     <BrowserRouter>
